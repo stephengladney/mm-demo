f45a0da4a66f146f3258fdb453ef309c92534b94
Stephen Gladney <stephengladney@gmail.com>
2023-06-27 18:20:37 -0400

    Better bare state. Turn on prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
   return (
-    // <ChakraProvider>
-    <div></div>
-    // </ChakraProvider>
+    <ChakraProvider>
+      <div></div>
+    </ChakraProvider>
   )
 }
 
