eca3b7c3fedf02092b1e150cd6398cc89add3fa5
Stephen Gladney <stephengladney@gmail.com>
2023-06-26 23:41:47 -0400

    Install MUI prefabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css"
-import { Button, ChakraProvider } from "@chakra-ui/react"
+// import { Button, ChakraProvider } from "@chakra-ui/react"
+import { Button, Switch } from "@mui/material"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 
 function App() {
   return (
-    <ChakraProvider>
-      <>
-        <Button>HELLO</Button>
-      </>
-    </ChakraProvider>
+    // <ChakraProvider>
+    <>
+      <Button>HELLO</Button>
+      <Switch></Switch>
+    </>
+    // </ChakraProvider>
   )
 }
 
