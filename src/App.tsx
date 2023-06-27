2127782aa06f87c8ded202c3cf64155361204d2c
Stephen Gladney <stephengladney@gmail.com>
2023-06-26 22:51:04 -0400

    Setup project. Add Chakra and prefabs. Install MUI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Button, ChakraProvider } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <ChakraProvider>
+      <>
+        <Button>HELLO</Button>
+      </>
+    </ChakraProvider>
+  )
+}
+
+export default App
